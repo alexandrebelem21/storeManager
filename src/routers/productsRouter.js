1879411ec4958abe18f7ab90c39d49e0050f3a2c
alexandrebelem21 <alexandrebelem21@gmail.com>
2023-02-19 04:27:57 -0300

    Feat: Create Middleware validade Name

--- src/routers/productsRouter.js
+++ src/routers/productsRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { productsController } = require('../controllers');
+const validateProductName = require('../middlewares/validateProductName');
 
 const productsRouter = express.Router();
 
 productsRouter.get('/', productsController.listProducts);
 productsRouter.get('/:id', productsController.getProduct);
-productsRouter.post('/', productsController.createProduct);
+productsRouter.post('/', validateProductName, productsController.createProduct);
 
 module.exports = productsRouter;
